d93f382f3206c7b6364cdcbc0b16f5edf5ba1189
Renan Reschke <renanreschke@hotmail.com>
2022-10-22 11:07:32 -0300

    reducers

--- index.js
+++ index.js
@@ -26,3 +26,30 @@ const solicitarCashback = (nome, valor) => {
         }
     }
 }
+
+const historicoDePedidosDeCashback = (historicoDePedidosDeCashbackAtual = [], acao) => {
+    if (acao.type === "CASHBACK") {
+        return [...historicoDePedidosDeCashbackAtual, acao.payload]
+    }
+    return historicoDePedidosDeCashbackAtual
+}
+
+const caixa = (dinheiroEmCaixa = 0, acao) => {
+    if (acao.type === "CRIAR_CONTRATO") {
+        return dinheiroEmCaixa + acao.payload.taxa
+    }
+    else if (acao.type === "CASHBACK") {
+        return dinheiroEmCaixa - acao.payload.valor
+    }
+    return dinheiroEmCaixa
+}
+
+const contratos = (contratosAtuais = [], acao) => {
+    if (acao.type === "CRIAR_CONTRATO") {
+        return [...contratosAtuais, acao.payload]
+    }
+    else if (acao.type === "CANCELAR_CONTRATO") {
+        return contratosAtuais.filter(contrato => contrato.nome !== acao.payload.nome)
+    }
+    return contratosAtuais
+}
